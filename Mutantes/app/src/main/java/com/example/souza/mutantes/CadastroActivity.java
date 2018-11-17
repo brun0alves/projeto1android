fbe5501bf1c76e946f3e0379a4157b61ee4beadf
Paulo Giovanni <paulo.rupp198@gmail.com>
2018-11-17 17:12:23 -0200

    added alert dialogs

--- Mutantes/app/src/main/java/com/example/souza/mutantes/CadastroActivity.java
+++ Mutantes/app/src/main/java/com/example/souza/mutantes/CadastroActivity.java
@@ -1,6 +1,8 @@
 package com.example.souza.mutantes;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -24,47 +26,66 @@ public class CadastroActivity extends AppCompatActivity {
     }
 
     public void addHab(View view) {
-        EditText habilidade = (EditText)findViewById(R.id.habilidade);
-        aux=habilidade.getText().toString();
-        if(!habilidades.equals(""))
-        habilidades = habilidades + ", " + aux;
+        EditText habilidade = (EditText) findViewById(R.id.habilidade);
+        aux = habilidade.getText().toString();
+        if (!habilidades.equals(""))
+            habilidades = habilidades + ", " + aux;
         else
             habilidades = aux;
     }
 
     public void finalizar(View view) {
+        AlertDialog alerta;
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        try {
+            Intent it = getIntent();
+            Bundle params = it.getExtras();
+            int aux2 = params.getInt("aux");
+            int id = params.getInt("mutanteId");
 
-        Intent it = getIntent();
-        Bundle params = it.getExtras();
-        int aux2 = params.getInt("aux");
-        int id = params.getInt("mutanteId");
+            EditText nome = (EditText) findViewById(R.id.nome);
+            if (!(habilidades.equals("") || nome.getText().toString().equals(""))) {
+                if (aux2 == 0) {
+                    List<Mutante> mutantes = mutanteDBOperations.getAllMutante();
+                    for (Mutante m : mutantes) {
+                        if (m.getNome().equalsIgnoreCase(nome.getText().toString())) {
+                            String e = "Mutante já cadastrado!";
+                            throw new Exception(e);
+                        }
+                    }
+                    mutanteDBOperations.addMutante(nome.getText().toString(), habilidades);
+                }
+                if (aux2 == 1) {
+                    List<Mutante> mutantes = mutanteDBOperations.getAllMutante();
+                    for (Mutante m : mutantes) {
+                        if (m.getNome().equalsIgnoreCase(nome.getText().toString())) {
+                            String e = "Mutante já cadastrado!";
+                            throw new Exception(e);
+                        }
+                    }
+                    mutanteDBOperations.updateMutante(nome.getText().toString(), habilidades, id);
+                }
+                builder.setTitle("Mutante Cadastrado!");
+                builder.setMessage("Cadastro do mutante " + nome.getText().toString() + "foi efetuado com sucesso!");
+                alerta = builder.create();
+                alerta.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    public void onDismiss(DialogInterface dialog) {
+                        finish();
+                    }
+                });
+                alerta.show();
+            } else {
+                String e = "Preencha todos os dados e adicione habilidades!";
+                throw new Exception(e);
+            }
+        } catch (Exception e) {
+            builder.setTitle("Um erro ocorreu!");
+            builder.setMessage("Não foi possível cadastrar o mutante." + "\n" + e.getMessage());
+            alerta = builder.create();
+            alerta.show();
 
-        EditText nome = (EditText)findViewById(R.id.nome);
-        if (!(habilidades.equals("") || nome.getText().toString().equals(""))) {
-            if (aux2 == 0) {
-                List<Mutante> mutantes = mutanteDBOperations.getAllMutante();
-                for (Mutante m : mutantes) {
-                    if (m.getNome().equalsIgnoreCase(nome.getText().toString())){
-                        Toast.makeText(this, "Nome de mutante já cadastrado!", Toast.LENGTH_SHORT).show();
-                        return;
-                    }
-                }
-                mutanteDBOperations.addMutante(nome.getText().toString(), habilidades);
-            }
-            if (aux2 == 1) {
-                List<Mutante> mutantes = mutanteDBOperations.getAllMutante();
-                for (Mutante m : mutantes) {
-                    if (m.getNome().equalsIgnoreCase(nome.getText().toString())){
-                        Toast.makeText(this, "Nome de mutante já cadastrado!", Toast.LENGTH_SHORT).show();
-                        return;
-                    }
-                }
-                mutanteDBOperations.updateMutante(nome.getText().toString(), habilidades, id);
-            }
-            finish();
         }
-        else
-            Toast.makeText(this, "Preencha todos os dados e adicione habilidades!", Toast.LENGTH_SHORT).show();
     }
 
     @Override
