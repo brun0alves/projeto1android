18abd7a9c59356668ba8f32fdd7a0c35ebfedcaf
Paulo Giovanni <paulo.rupp198@gmail.com>
2018-11-17 17:31:34 -0200

    added delete alert dialog

--- Mutantes/app/src/main/java/com/example/souza/mutantes/DetalhesActivity.java
+++ Mutantes/app/src/main/java/com/example/souza/mutantes/DetalhesActivity.java
@@ -1,6 +1,8 @@
 package com.example.souza.mutantes;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,16 +19,35 @@ public class DetalhesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detalhes);
     }
 
-    public void deletar (View view) {
-        Intent it = getIntent();
-        Bundle params = it.getExtras();
-        int mutanteId = params.getInt("mutanteId");
-        mutanteDBOperations.deleteMutante(mutanteId);
-        finish();
+    public void deletar(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        AlertDialog alerta;
+        try {
+            Intent it = getIntent();
+            Bundle params = it.getExtras();
+            int mutanteId = params.getInt("mutanteId");
+            builder.setTitle("Mutante Deletado!");
+            builder.setMessage("O mutante " + mutanteDBOperations.getMutanteById(mutanteId).getNome() + " foi deletado com sucesso!");
+            mutanteDBOperations.deleteMutante(mutanteId);
+            alerta = builder.create();
+            alerta.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                @Override
+                public void onDismiss(DialogInterface dialog) {
+                    finish();
+                }
+            });
+            alerta.show();
+        } catch (Exception e) {
+            builder.setTitle("Um erro ocorreu!");
+            builder.setMessage("Não foi possível deletar o mutante." + "\n" + e.getMessage());
+            alerta = builder.create();
+            alerta.show();
+
+        }
 
     }
 
-    public void editar (View view) {
+    public void editar(View view) {
         int aux = 1;
 
         Intent it = getIntent();
